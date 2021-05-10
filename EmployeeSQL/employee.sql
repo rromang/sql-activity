d28863ab54a9e38b6e3eb509130419f1c31392a7
rromang <80008461+rromang@users.noreply.github.com>
2021-05-09 19:02:19 -0700

    Update employee.sql

--- EmployeeSQL/employee.sql
+++ EmployeeSQL/employee.sql
@@ -1,6 +1,6 @@
 
 --Remove employee table if exists
-drop table "Employees"
+drop table "Employees";
 
 
 --Create Table with employee information
@@ -18,3 +18,41 @@ CREATE TABLE "Employees" (
      )
 );
 --Import CSV
+--Quick View of imported data
+select *
+from "Employees"
+limit 5;
+
+--Remove titles table if exists
+drop table "Titles";
+
+--Create Titles table
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(30)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+--Import CSV
+--Quick View of imported data
+select *
+from "Titles"
+limit 5;
+
+--Remove titles table if exists
+drop table "Department_Mgr";
+
+
+CREATE TABLE "Department_Mgr" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(30)   NOT null,
+    primary key ("emp_no","dept_no")
+);
+
+--Import CSV
+--Quick View of imported data
+select *
+from "Department_Mgr"
+limit 5;
