7ac42d64cd36557860431392152235dad215a75d
rromang <80008461+rromang@users.noreply.github.com>
2021-05-14 20:38:58 -0700

    Created 2 separate files for the tables and the queries

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -280,7 +280,7 @@ SELECT
 		last_name,
 		sex,
 		salary
-FROM employee_info;
+FROM employee_info_new;
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 SELECT
@@ -288,7 +288,7 @@ SELECT
 		first_name,
 		last_name,
 		hire_date
-FROM employee_info
+FROM employee_info_new
 WHERE hire_date LIKE '%1986';
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
@@ -305,6 +305,32 @@ SELECT
 		first_name,
 		last_name,
 		sex
-FROM employee_info
+FROM employee_info_new
 WHERE first_name LIKE 'Hercules'
 AND last_name LIKE 'B%';
+
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT
+		emp_no,
+		last_name,
+		first_name,
+		dept_name		
+FROM employee_info_new
+WHERE dept_name LIKE 'Sales';
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT
+		emp_no,
+		last_name,
+		first_name,
+		dept_name		
+FROM employee_info_new
+WHERE dept_name LIKE 'Sales' OR dept_name LIKE 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+SELECT COUNT(emp_no), last_name
+FROM employee_info_new
+GROUP BY employee_info_new.last_name
+ORDER BY COUNT(emp_no) DESC;
+
